03ada848d1a8d3e9a1004a50b015c653bb6feaff
Yuichiro Tanaka <tanaka.yuichiro483@mail.kyutech.jp>
2019-01-22 16:13:17 +0900

    start bias-weight

--- src/Network/BiasWeight.v
+++ src/Network/BiasWeight.v
@@ -5,24 +5,79 @@ module BiasWeight #
 , parameter WI     = 4
 , parameter WF     = 4
 )
-( input                       iMode
-, input                       iValid_AS_State1
-, output                      oReady_AS_State1
-, input           [NP*WF-1:0] iData_AS_State1
-, input                       iValid_AS_Delta1
-, output                      oReady_AS_Delta1
-, input              [WD-1:0] iData_AS_Delta1
-, output                      oValid_BM_WeightBias
-, input                       iReady_BM_WeightBias
-, output [NC*NP*WF+NC*WF-1:0] oData_BM_WeightBias
-, output                      oValid_BM_Weight
-, input                       iReady_BM_Weight
-, output       [NC*NP*WF-1:0] oData_BM_Weight
-, input                       iRST
-, input                       iCLK
+( input                                                               iMode
+, input                                                      [WD-1:0] iLR
+, input                                                               iValid_AS
+, output                                                              oReady_AS
+, input  [NP*WD+(HIDDEN=="yes")?NC*($clog2(NN)+WC+WD):NC*(WC+WD)-1:0] iData_AS
+, output                                                              oValid_BS
+, input                                                               iReady_BS
+, output                                    [NP*NC*WD+NC*WD+NC*NP*WD] oData_BS
+, input                                                               iRST
+, input                                                               iCLK
 );
 
-localparam WD =
-    (HIDDEN == "yes") ? NC * ($clog2(NN) + WI + WF) : NC * (WI + WF);
+genvar gi;
+
+// pipeline 0
+wire signed             [(HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)-1:0] w_dc  [0:NC-1];
+wire signed             [(HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)-1:0] w_adc [0:NC-1];    // bit width ?
+wire        [NP*WD+(HIDDEN=="yes")?NC*($clog2(NN)+WC+WD):NC*(WC+WD)-1:0] w_dat_as0;
+
+assign w_dat_as0[NP*WD-1:0] = iData_AS[NP*WD-1:0];
+
+generate
+for (gi = 0; gi < NC; gi = gi + 1) begin : w_adc
+    assign w_dc[gi] = iData_AS[NP*WD + gi*((HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)) +: (HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)];
+    assign w_adc[gi] = iLR * w_dc[gi];
+    assign w_dat_as0[NP*WD + gi*((HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)) +: (HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)] = w_adc[gi];
+end
+endgenerate
+
+wire w_vld_as0;
+wire w_rdy_as0;
+
+assign 
+
+PipelineRegister #
+( .WD           (NP*WD+(HIDDEN=="yes")?NC*($clog2(NN)+WC+WD):NC*(WC+WD))
+) pipeline_register0
+( .iValid_AS    (w_vld_as0)
+, .oReady_AS    (w_rdy_as0)
+, .iData_AS     (w_dat_as0)
+, .oValid_BS    (w_vld_bs0)
+, .iReady_BS    (w_rdy_bs0)
+, .oData_BS     ()
+, .iRST         (iRST)
+, .iCLK         (iCLK)
+);
+
+// pipeline 1
+PipelineRegister #
+( .WD           ()
+) pipeline_register1
+( .iValid_AS    ()
+, .oReady_AS    ()
+, .iData_AS     ()
+, .oValid_BS    ()
+, .iReady_BS    ()
+, .oData_BS     ()
+, .iRST         (iRST)
+, .iCLK         (iCLK)
+);
+
+// pipeline 2
+PipelineRegister #
+( .WD           ()
+) pipeline_register2
+( .iValid_AS    ()
+, .oReady_AS    ()
+, .iData_AS     ()
+, .oValid_BS    ()
+, .iReady_BS    ()
+, .oData_BS     ()
+, .iRST         (iRST)
+, .iCLK         (iCLK)
+);
 
 endmodule
---
+++ src/Network/PipelineRegister.v
@@ -0,0 +1,34 @@
+module PipelineRegister #
+( parameter WD = 4
+)
+( input             iValid_AS
+, output            oReady_AS
+, input    [WD-1:0] iData_AS
+, output            oValid_BS
+, input             iReady_BS
+, output   [WD-1:0] oData_BS
+, input             iRST
+, input             iCLK
+)
+
+reg  [WD-1:0] r_stl;
+reg  r_vld;
+wire w_rdy
+
+assign oData_BS = r_stl;
+assign oValid_BS = r_vld;
+assign oReady_AS = w_rdy;
+assign w_rdy = (r_vld) ? iReady_BS : 1;
+
+always @(posedge iCLK) begin
+    if (iRST) begin
+        r_stl <= 0;
+        r_vld <= 0;
+    end
+    else begin
+        r_stl <= (iValid_AS && w_rdy) ? iData_AS : r_stl;
+        r_vld <= (r_vld) ? !((!iValid_AS) && w_rdy) : iValid_AS;
+    end
+end
+
+endmodule
