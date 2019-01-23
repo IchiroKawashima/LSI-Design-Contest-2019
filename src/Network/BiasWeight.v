5d589eff0a7d720ebd5b5296703acb05b300ee18
Yuichiro Tanaka <tanaka.yuichiro483@mail.kyutech.jp>
2019-01-23 15:09:44 +0900

    describe until pipeline0

--- src/Network/BiasWeight.v
+++ src/Network/BiasWeight.v
@@ -5,7 +5,7 @@ module BiasWeight #
 , parameter WI     = 4
 , parameter WF     = 4
 )
-( input                                                               iMode
+( input                                                               iMode // 0: inference, 1: training ?
 , input                                                      [WD-1:0] iLR
 , input                                                               iValid_AS
 , output                                                              oReady_AS
@@ -20,24 +20,28 @@ module BiasWeight #
 genvar gi;
 
 // pipeline 0
-wire signed             [(HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)-1:0] w_dc  [0:NC-1];
-wire signed             [(HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)-1:0] w_adc [0:NC-1];    // bit width ?
+wire signed             [(HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)-1:0] w_dc   [0:NC-1];
+wire signed             [(HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)-1:0] w_adc0 [0:NC-1];    // bit width ?
 wire        [NP*WD+(HIDDEN=="yes")?NC*($clog2(NN)+WC+WD):NC*(WC+WD)-1:0] w_dat_as0;
 
 assign w_dat_as0[NP*WD-1:0] = iData_AS[NP*WD-1:0];
 
 generate
-for (gi = 0; gi < NC; gi = gi + 1) begin : w_adc
+for (gi = 0; gi < NC; gi = gi + 1) begin : w_adc0
     assign w_dc[gi] = iData_AS[NP*WD + gi*((HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)) +: (HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)];
-    assign w_adc[gi] = iLR * w_dc[gi];
-    assign w_dat_as0[NP*WD + gi*((HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)) +: (HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)] = w_adc[gi];
+    assign w_adc0[gi] = iLR * w_dc[gi];
+    assign w_dat_as0[NP*WD + gi*((HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)) +: (HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)] = w_adc0[gi];
 end
 endgenerate
 
 wire w_vld_as0;
 wire w_rdy_as0;
+wire w_vld_bs0;
+wire w_rdy_bs0;
+wire [NP*WD+(HIDDEN=="yes")?NC*($clog2(NN)+WC+WD):NC*(WC+WD)-1:0] w_dat_bs0;
 
-assign 
+assign w_vld_as0 = iValid_AS && iMode;
+assign oReady_AS = (iMode) ? w_rdy_as0 : w_rdy_as2;
 
 PipelineRegister #
 ( .WD           (NP*WD+(HIDDEN=="yes")?NC*($clog2(NN)+WC+WD):NC*(WC+WD))
@@ -47,20 +51,30 @@ PipelineRegister #
 , .iData_AS     (w_dat_as0)
 , .oValid_BS    (w_vld_bs0)
 , .iReady_BS    (w_rdy_bs0)
-, .oData_BS     ()
+, .oData_BS     (w_dat_bs0)
 , .iRST         (iRST)
 , .iCLK         (iCLK)
 );
 
 // pipeline 1
+wire signed             [(HIDDEN=="yes")?($clog2(NN)+WC+WD):(WC+WD)-1:0] w_adc1 [0:NC-1]; // bit width ?
+
+wire w_vld_as1;
+wire w_rdy_as1;
+wire w_vld_bs1;
+wire w_rdy_bs1;
+
+assign w_vld_as1 = w_vld_bs0;
+assign w_rdy_bs0 = w_rdy_as1;
+
 PipelineRegister #
 ( .WD           ()
 ) pipeline_register1
-( .iValid_AS    ()
-, .oReady_AS    ()
+( .iValid_AS    (w_vld_as1)
+, .oReady_AS    (w_rdy_as1)
 , .iData_AS     ()
-, .oValid_BS    ()
-, .iReady_BS    ()
+, .oValid_BS    (w_vld_bs1)
+, .iReady_BS    (w_rdy_bs1)
 , .oData_BS     ()
 , .iRST         (iRST)
 , .iCLK         (iCLK)
